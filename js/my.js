0a8237273f8eb4db9ddf1ae3640512ce69bcebba
Ahmad <81382585+ahmadarif001@users.noreply.github.com>
2021-06-10 22:59:19 +0500

    Added Files

---
+++ js/my.js
@@ -0,0 +1,4 @@
+		var elementsToShow = document.querySelectorAll('.show-on-scroll');
+        function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;}
